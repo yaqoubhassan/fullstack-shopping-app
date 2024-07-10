92eacc6a3e5ff58f1229d98e6bd2746c8d7a8064
Yakubu Alhassan <yaqoubdramani@gmail.com>
2024-07-10 21:42:43 +0000

    Update vue router

--- resources/js/router/index.js
+++ resources/js/router/index.js
@@ -1,14 +1,16 @@
 import { createRouter, createWebHistory } from 'vue-router';
+import Home from '../views/Home.vue';
 import ProductsList from '../views/ProductsList.vue';
-// import ProductDetails from '../views/ProductDetails.vue';
-// import InvoicesList from '../views/InvoicesList.vue';
-// import InvoiceDetails from '../views/InvoiceDetails.vue';
+import ProductDetails from '../views/ProductDetails.vue';
+import InvoicesList from '../views/InvoicesList.vue';
+import InvoiceDetails from '../views/InvoiceDetails.vue';
 
 const routes = [
-  { path: '/', component: ProductsList },
-  // { path: '/products/:id', component: ProductDetails },
-  // { path: '/invoices', component: InvoicesList },
-  // { path: '/invoices/:id', component: InvoiceDetails },
+  { path: '/', component: Home },
+  { path: '/products', component: ProductsList },
+  { path: '/products/:id', component: ProductDetails },
+  { path: '/invoices', component: InvoicesList },
+  { path: '/invoices/:id', component: InvoiceDetails },
 ];
 
 const router = createRouter({
