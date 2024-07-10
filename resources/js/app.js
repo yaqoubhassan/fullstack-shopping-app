42503864c1d5c6d80426bcb141b85daed615e360
Yakubu Alhassan <yaqoubdramani@gmail.com>
2024-07-10 22:08:10 +0000

    Configure pinia

--- resources/js/app.js
+++ resources/js/app.js
@@ -2,8 +2,13 @@ import './bootstrap';
 import { createApp } from 'vue';
 import App from './App.vue';
 import router from './router';
+import { createPinia } from 'pinia';
 
-createApp(App)
-  .use(router)
-  .mount('#app');
+
+const app = createApp(App);
+const pinia = createPinia();
+
+app.use(pinia);
+app.use(router);
+app.mount('#app');
 
--- vite.config.js
+++ vite.config.js
@@ -15,4 +15,7 @@ export default defineConfig({
             vue: "vue/dist/vue.esm-bundler.js",
         },
     },
+    optimizeDeps: {
+        include: ['axios'],
+    },
 });
