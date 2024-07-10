1cd609e99ed77d19905aee661db4e2b81ce189da
Yakubu Alhassan <yaqoubdramani@gmail.com>
2024-07-10 21:33:25 +0000

    Create invoice item model and define relationship

---
+++ app/Models/InvoiceItem.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class InvoiceItem extends Model
+{
+    use HasFactory;
+
+    public function invoice()
+    {
+        return $this->belongsTo(Invoice::class);
+    }
+
+    public function product()
+    {
+        return $this->belongsTo(Product::class);
+    }
+}
